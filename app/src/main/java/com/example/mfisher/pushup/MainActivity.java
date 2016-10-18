f4699bbe660888c21c0603255299fba98f952b10
mitchfish <mfisher@gips.org>
2016-10-18 09:48:39 -0500

    Modified MainActivity Added New File TestAddFile.java

--- app/src/main/java/com/example/mfisher/pushup/MainActivity.java
+++ app/src/main/java/com/example/mfisher/pushup/MainActivity.java
@@ -15,5 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     int i = 1 + 1;
+
+        int ii = 2 + 2;
+
+        int iii = i + ii;
     }
 }
---
+++ app/src/main/java/com/example/mfisher/pushup/TestAddFile.java
@@ -0,0 +1,8 @@
+package com.example.mfisher.pushup;
+
+/**
+ * Created by mfisher on 10/18/2016.
+ */
+
+public class TestAddFile {
+}
